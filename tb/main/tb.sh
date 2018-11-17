397b02cbc40f498b5ceb30f64a2db1dd17e002b7
Giuseppe <s255648@studenti.polito.it>
2018-11-17 09:23:12 +0100

    bug fixies in the tb

--- tb/main/tb.sh
+++ tb/main/tb.sh
@@ -5,12 +5,11 @@ echo "Compiling..."
 gcc -Wall read.c draw.c tb.c -lm main.c -o run.ex
 chmod +x run.ex
 echo "Do you want to execute the program tb? (Y/n)"
-read line
-if (($line==n)); then
+    read line
+if [[ $line == "n" ]]; then
     exit 0
 else
     if [[ -f ./run.ex ]]; then
-        echo "halo"
 	    ./run.ex
     else
 	    echo "Can't run execution file. File not present!"
